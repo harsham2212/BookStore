8c3dbd19788e9d76eb6e3442a2de153e056bc867
Harsh Singh <kmrharsh50@gmail.com>
2022-03-11 11:25:36 +0530

    [Harsh]Added:CRUD operations perform in User API and Book API and many features implemented Book-Store feature implemented

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,139 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        IBookBL bookBL;
+        public BookController(IBookBL bookBL)
+        {
+            this.bookBL = bookBL;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("addBooks")]
+        public IActionResult AddBook(BookModel book)
+        {
+            try
+            {
+                string result = this.bookBL.AddBook(book);
+                if (result != null)
+                {
+                    return this.Ok(new { success = true, message = $"Book Added Successfully" });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = $"Book Added UnSuccessfull"});
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = false, Message = e.Message });
+            }
+        }
+
+        [Authorize]
+        [HttpPut]
+        [Route("updateBook/{BookId}")]
+        public IActionResult UpdateBookDetails(UpdateBookModel update, int BookId)
+        {
+            try
+            {
+                string result = this.bookBL.UpdateBookDetails(update,BookId);
+                if (result.Equals(true))
+                {
+                    return this.BadRequest(new { Status = false, Message = $"Book not updated" });     
+                }
+                else
+                {
+                    return this.Ok(new { success = true, message = $"Book Update Successfully" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = false, Message = e.Message });
+            }
+        }
+
+        [Authorize]
+        [HttpDelete]
+        [Route("deleteBook/{BookId}")]
+        public IActionResult DeleteBook(int BookId)
+        {
+            try
+            {
+                string result = this.bookBL.DeleteBook(BookId);
+                if (result.Equals(true))
+                {
+                    return this.BadRequest(new { Status = false, Message = $"Unable To Delete" });
+                }
+                else
+                {
+                    return this.Ok(new { success = true, message = $"Delete Successfully" });
+                   
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = false, Message = e.Message });
+            }
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("getAllBooks")]
+        public IActionResult GetAllBooks()
+        {
+            try
+            {
+                var result = this.bookBL.GetAllBooks();
+                if (result != null)
+                {
+                    return this.Ok(new { Status = true, Message = " All book details Given", Data = result });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = "No book exists"});
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = false, Message = e.Message });
+            }
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("getAllBooksbyBookId/{BookId}")]
+        public IActionResult GetAllBooksByBookId(int BookId)
+        {
+            try
+            {
+                //var BookList = new List<BookModel>();
+                var BookList = this.bookBL.GetAllBooksbyBookId(BookId);
+                if (BookList != null)
+                {
+                    return this.Ok(new { Status = true, Message = "Book details of given ID is Sucessfull", Data = BookList });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = "BookId doesn't exists" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = false, Message = e.Message });
+            }
+        }
+    }
+}
